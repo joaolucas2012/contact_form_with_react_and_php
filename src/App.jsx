7a66f1edaf3f8c4e9c60eb5dc8f6708ddada59e2
joaolucas2012 <ribeirojoaolucas68@gmail.com>
2022-11-09 00:30:52 -0300

    :fire: [FIX] Implement a class component for App and add event handler to the form

--- src/App.jsx
+++ src/App.jsx
@@ -1,45 +1,75 @@
+import React, { Component } from "react";
 import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <p>Contact Me</p>
-      <div>
-        <form action="/action_page.php">
-          <label>First Name</label>
-          <input
-            type="text"
-            id="fname"
-            name="firstname"
-            placeholder="Your name.."
-          />
-          <label>Last Name</label>
-          <input
-            type="text"
-            id="lname"
-            name="lastname"
-            placeholder="Your last name.."
-          />
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      fname: "",
+      lname: "",
+      email: "",
+      message: "",
+    };
+  }
 
-          <label>Email</label>
-          <input
-            type="email"
-            id="email"
-            name="email"
-            placeholder="Your email"
-          />
+  handleFormSubmit(event) {
+    event.preventDefault();
+    console.log(this.state);
+  }
 
-          <label>Subject</label>
-          <textarea
-            id="subject"
-            name="subject"
-            placeholder="Write something.."
-          ></textarea>
-          <input type="submit" value="Submit" />
-        </form>
+  render() {
+    return (
+      <div className="App">
+        <p>Contact Me</p>
+        <div>
+          <form action="#">
+            <label>First Name</label>
+            <input
+              type="text"
+              id="fname"
+              name="firstname"
+              placeholder="Your name.."
+              value={this.state.fname}
+              onChange={(e) => this.setState({ fname: e.target.value })}
+            />
+            <label>Last Name</label>
+            <input
+              type="text"
+              id="lname"
+              name="lastname"
+              placeholder="Your last name.."
+              value={this.state.lname}
+              onChange={(e) => this.setState({ lname: e.target.value })}
+            />
+
+            <label>Email</label>
+            <input
+              type="email"
+              id="email"
+              name="email"
+              placeholder="Your email"
+              value={this.state.email}
+              onChange={(e) => this.setState({ email: e.target.value })}
+            />
+
+            <label>Message</label>
+            <textarea
+              id="message"
+              name="message"
+              placeholder="Write something.."
+              onChange={(e) => this.setState({ message: e.target.value })}
+              value={this.state.message}
+            ></textarea>
+            <input
+              type="submit"
+              onClick={(e) => this.handleFormSubmit(e)}
+              value="Submit"
+            />
+          </form>
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default App;
